4c689027055bb469f53a98932789be41bd4b081b
Alejo <jubany4@gmail.com>
2023-11-30 13:37:45 -0300

    instalacion de react-icons y creacion searchBar

--- mystore/app/Components/Navbar.tsx
+++ mystore/app/Components/Navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
+import { CiShoppingCart } from "react-icons/ci";
+import Link from "next/link";
 
 type Props = {};
 
@@ -8,12 +10,13 @@ const Navbar = (props: Props) => {
   const [showProfile, setshowProfile] = useState<boolean>(false);
   const [showNav, setshowNav] = useState<boolean>(false);
   return (
+    <div>
     <div className="flex items-center justify-between py-4 relative">
       <div className="flex items-center md:space-x-10 lg:space-x-20">
         <div className="font-semibold text-2xl">
           <a href="/">Seine</a>
         </div>
-        <nav className="max-md_hidden">
+        <nav className="max-md:hidden">
           <ul className="flex items-center lg:space-x-10 space-x-7 opacity-70 text-[15px]">
             <li>
               <a href="/" className="py-3 inline-block w-full">
@@ -34,8 +37,19 @@ const Navbar = (props: Props) => {
         </nav>
       </div>
       <div className="flex items-center space-x-4">
-        <SearchBar/>
+        <SearchBar />
+        <div className="relative cursor-pointer">
+            <img src="user.jpg" className="w-[35px] h-[35px] rounded-full object-cover" alt=""/>
+            <div className="absolute bg-white z-[2] rounded-lg shadow-lg">
+                <Link href="/sign">SignIn</Link>
+            </div>
+            <Link href="/cart">
+            <div className="p-2 bg-gray-100 rounded-full"><CiShoppingCart size={20}/>
+                </div>
+                </Link>
+        </div>
       </div>
+    </div>
     </div>
   );
 };
